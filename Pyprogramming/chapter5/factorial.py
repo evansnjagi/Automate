2d1197561daf5d6f2e44b03a58b051b71a6b16e1
Evanskarago <148349906+evansnjagi@users.noreply.github.com>
2026-01-29 09:48:49 +0000

    Added a factorial using recursion technique

---
+++ Pyprogramming/chapter5/factorial.py
@@ -0,0 +1,18 @@
+# Import modules
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s -  %(levelname)s -  %(message)s')
+logging.debug("Starting the program")
+
+x = int(input("Enter number: "))
+logging.debug("Get user's input")
+def f(x):
+    # Base case
+    if x == 0:
+        logging.debug(f"Recursive base case")
+        return 1
+    logging.debug(f"Recursion: {x}")
+    return x * f(x - 1)
+    logging.debug("Ending the function")
+
+print(f"Factorial of {x} is: {f(x)}")
